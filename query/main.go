f1fcb4e5721cf72a806f546bfb7d6344a1823405
Jens Gerke <jens.gerke@direktiv.io>
2025-02-27 10:29:43 +0100

    lint
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

---
+++ query/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	// "github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported"
+
+	da "github.com/direktiv/direktiv-apps/pkg/direktivapps"
+)
+
+type input struct {
+	Query struct {
+		Policies interface{} `json:"policies"`
+		User     interface{} `json:"user"`
+	} `json:"query"`
+}
+
+const (
+	errCode = "com.azure.%s"
+)
+
+func main() {
+	da.StartServer(coreLogic)
+}
+
+func reportError(w http.ResponseWriter, code string, err error) {
+	da.RespondWithError(w, fmt.Sprintf(errCode, code), err.Error())
+}
+
+func coreLogic(w http.ResponseWriter, r *http.Request) {
+	obj := new(input)
+	aid, err := da.Unmarshal(obj, r)
+	if err != nil {
+		fmt.Println(err)
+		reportError(w, "inputUnmarshal", err)
+		return
+	}
+
+	fmt.Println(aid)
+	da.LogDouble("%s", aid)
+
+	w.Write([]byte("{ \"where\": \"true\"}"))
+}
