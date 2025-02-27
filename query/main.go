a4a769b3118f95783fb2a4a846153fe1fdf39e05
sir-hassan <yassir.hassan@direktiv.io>
2025-02-27 15:52:42 +0100

    parse the request

--- query/main.go
+++ query/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"query/pkg/rulejson"
 
 	// "github.com/Azure/azure-sdk-for-go/sdk/azcore/internal/exported"
 
 	da "github.com/direktiv/direktiv-apps/pkg/direktivapps"
 )
 
+type UserAttribute struct {
+	Description  string `json:"description"`
+	Name         string `json:"name"`
+	Value        string `json:"value"`
+	ID           any    `json:"id"`
+	SrcAttr      any    `json:"srcAttr"`
+	SrcDirectory any    `json:"srcDirectory"`
+	SrcName      any    `json:"srcName"`
+	Typ          any    `json:"type"`
+	RegExp       any    `json:"regExp"`
+	Project      any    `json:"project"`
+}
+
 type input struct {
 	Query struct {
-		Policies interface{} `json:"policies"`
-		User     interface{} `json:"user"`
+		Policies []rulejson.Rule `json:"policies"`
+		User     []UserAttribute `json:"user"`
 	} `json:"query"`
 }
 
